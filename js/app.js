8e7816cac69ef46de2f45ac6020369cc9f7c8588
cyeguez <cesar.yeguez@gmail.com>
2023-11-03 18:17:21 -0300

    feat(Styles de ambas vistas culminados con js)

---
+++ js/app.js
@@ -0,0 +1,53 @@
+const btnMenu = document.querySelector('.header__btn-menu');
+const btnClose = document.querySelector('.header__btn-close');
+const menu = document.querySelector('.header__menu');
+const arrowFeactures = document.getElementById('arrowFeactures');
+const arrowCompany = document.getElementById('arrowCompany');
+const listFeactures = document.querySelector('.header__menu__list__sub-list');
+const listCompany = document.querySelector('.header__menu__list__sub-list--list2');
+
+
+btnMenu.addEventListener('click',show); // Añadir evento click al boton de menu
+btnClose.addEventListener('click',hidden);//Añadir evento click al boton de cerrar menu
+
+arrowFeactures.addEventListener('click',(e)=>{ //despliegue de la lista de feactures al presionar la flecha
+  let event = e;
+  showList(arrowFeactures, listFeactures,event);
+});
+arrowCompany.addEventListener('click',(e)=>{ //despliegue de la lista de feactures al presionar la flecha
+  let event = e;
+  showList(arrowCompany, listCompany,event);
+});
+
+function show() { //funcion para mostrar el menu 
+  btnMenu.style.display = 'none'; //oculto el boton del menu
+  btnClose.style.display = 'block'; //muestro el boton de cerrar menu
+  menu.classList.add('active'); //muesto el menu activo
+
+}
+
+function hidden() {
+  btnClose.style.display = 'none'; //oculto el boton de cerrar menu
+  btnMenu.style.display = 'block'; //muestro el boton del menu
+  menu.classList.remove('active');// oculto el menu activo
+}
+
+
+
+
+function showList(nameArrow, nameList,event ) {
+  if(event.target.classList.contains('rotate')){
+   
+    nameArrow.classList.remove('rotate'); 
+    nameList.style.display = 'none';    
+    
+    
+  }else{    
+    nameArrow.classList.add('rotate'); 
+    nameList.style.display = 'flex';
+    
+  }
+
+
+
+}
